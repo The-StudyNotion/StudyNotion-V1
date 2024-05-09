d73a64452b81f8703d2d9edb8bcb00ad27267664
hRJ06 <hindolroy.2306@gmail.com>
2024-05-09 21:09:31 +0530

    Updated the Enrolled Course Page for user

--- Client/src/Component/Core/Dashboard/EnrolledCourses.jsx
+++ Client/src/Component/Core/Dashboard/EnrolledCourses.jsx
@@ -12,7 +12,7 @@ export default function EnrolledCourses() {
   const [enrolledCourses, setEnrolledCourses] = useState(null)
 
   useEffect(() => {
-    ;(async () => {
+    ; (async () => {
       try {
         const res = await getUserEnrolledCourses(token) // Getting all the published and the drafted courses
 
@@ -33,7 +33,7 @@ export default function EnrolledCourses() {
 
   return (
     <>
-      <div className="text-3xl text-richblack-50">Enrolled Courses</div>
+      <div className="text-3xl text-richblack-50 uppercase tracking-wider lg:text-left text-center">Enrolled Course</div>
       {!enrolledCourses ? (
         <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
           <div className="spinner"></div>
@@ -41,57 +41,61 @@ export default function EnrolledCourses() {
       ) : !enrolledCourses.length ? (
         <p className="grid h-[10vh] w-full place-content-center text-richblack-5">
           You have not enrolled in any course yet.
-          {/* TODO: Modify this Empty State */}
         </p>
       ) : (
-        <div className="my-8 text-richblack-5">
-          {/* Headings */}
-          <div className="flex rounded-t-lg bg-richblack-500 ">
-            <p className="w-[45%] px-5 py-3">Course Name</p>
-            <p className="w-1/4 px-2 py-3">Duration</p>
-            <p className="flex-1 px-2 py-3">Progress</p>
-          </div>
-          {/* Course Names */}
-          {enrolledCourses.map((course, i, arr) => (
-            <div
-              className={`flex items-center border border-richblack-700 ${
-                i === arr.length - 1 ? "rounded-b-lg" : "rounded-none"
-              }`}
-              key={i}
-            >
+        <div className="overflow-x-auto">
+          <div className='my-8 text-richblack-5 w-[650px] md:w-full'>
+            {/* Headings */}
+            <div className="flex rounded-t-lg bg-richblack-500 ">
+              <p className="w-[45%] px-5 py-3 uppercase tracking-wider">Course Name</p>
+              <p className="w-1/4 px-2 py-3 uppercase tracking-wider">Duration</p>
+              <p className="flex-1 px-2 py-3 uppercase tracking-wider">Progress</p>
+            </div>
+            {/* Course Names */}
+            {enrolledCourses.map((course, i, arr) => (
               <div
-                className="flex w-[45%] cursor-pointer items-center gap-4 px-5 py-3"
-                onClick={() => {
-                  navigate(
-                    `/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.subSection?.[0]?._id}`
-                  )
-                }}
+                className={`flex items-center border border-richblack-700 ${i === arr.length - 1 ? "rounded-b-lg" : "rounded-none"
+                  }`}
+                key={i}
               >
-                <img
-                  src={course.thumbnail}
-                  alt="course_img"
-                  className="h-14 w-14 rounded-lg object-cover"
-                />
-                <div className="flex max-w-xs flex-col gap-2">
-                  <p className="font-semibold">{course.courseName}</p>
-                  <p className="text-xs text-richblack-300">
-                    {course.courseDescription.length > 50
-                      ? `${course.courseDescription.slice(0, 50)}...`
-                      : course.courseDescription}
-                  </p>
+                <div
+                  className="flex w-[45%] cursor-pointer items-center gap-4 px-5 py-3"
+                  onClick={() => {
+                    navigate(
+                      `/view-course/${course?._id}/section/${course.courseContent?.[0]?._id}/sub-section/${course.courseContent?.[0]?.subSection?.[0]?._id}`
+                    )
+                  }}
+                >
+                  <img
+                    src={course.thumbnail}
+                    alt="course_img"
+                    className="h-14 w-14 rounded-lg object-cover"
+                  />
+                  <div className="flex max-w-xs flex-col gap-2">
+                    <p className="font-semibold uppercase tracking-wider">{course.courseName}</p>
+                    <ul style={{ listStyle: 'none', padding: 0 }} className="tracking-wider">
+                      {course.courseDescription.split('\n').splice(0, 1).map((line, index) => (
+                        <li key={index} style={{ display: 'flex', alignItems: 'flex-start' }} className="text-xs text-richblack-300">
+                          <span style={{ marginRight: '0.5em' }}>{index + 1}.</span>
+                          <span>{line.trim().substring(line.indexOf('.') + 1).trim().slice(0, 50)}{line.length > 50 ? '...' : ''}</span>
+                        </li>
+                      ))}
+                    </ul>
+
+                  </div>
+                </div>
+                <div className="w-1/4 px-2 py-3 tracking-wider uppercase">{course?.totalDuration}</div>
+                <div className="flex w-1/5 flex-col gap-2 px-2 py-3 tracking-wider uppercase">
+                  <p>Progress - {course.progressPercentage || 0}%</p>
+                  <ProgressBar
+                    completed={course.progressPercentage || 0}
+                    height="8px"
+                    isLabelVisible={false}
+                  />
                 </div>
               </div>
-              <div className="w-1/4 px-2 py-3">{course?.totalDuration}</div>
-              <div className="flex w-1/5 flex-col gap-2 px-2 py-3">
-                <p>Progress: {course.progressPercentage || 0}%</p>
-                <ProgressBar
-                  completed={course.progressPercentage || 0}
-                  height="8px"
-                  isLabelVisible={false}
-                />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       )}
     </>
